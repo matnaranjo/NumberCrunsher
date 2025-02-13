65c236d0c0b415d2090d9bff745e7d1775f52104
matnaranjo <matnaranjo2020@gmail.com>
2025-02-12 22:59:53 -0400

    Hp working
    HP displayed and working to disable track

--- Assets/Scripts/Managers/TrackController.cs
+++ Assets/Scripts/Managers/TrackController.cs
@@ -8,6 +8,7 @@ public class TrackController : MonoBehaviour
     
     TextMeshProUGUI guesses;
     TextMeshProUGUI guessResult;
+    TextMeshProUGUI hpText;
     TextMeshProUGUI[] textObjects;
     TMP_InputField userGuess;
     Button submitButton;
@@ -25,7 +26,8 @@ public class TrackController : MonoBehaviour
         foreach (TextMeshProUGUI textObject in textObjects)
         {
             guesses = textObject.name == "Tries" ? textObject : guesses;
-            guessResult = textObject.name == "CorrectOrNot" ? textObject : guesses;
+            guessResult = textObject.name == "CorrectOrNot" ? textObject : guessResult;
+            hpText = textObject.name == "Hp" ? textObject : hpText;
         }
         userGuess = gameObject.transform.GetComponentInChildren<TMP_InputField>();
         submitButton = gameObject.transform.GetComponentInChildren<Button>();
@@ -37,6 +39,7 @@ public class TrackController : MonoBehaviour
         hp = gm.HP;
         range = gm.NumberLimit;
         numToGuess = NumGenerator.GenerateNumber(range);
+        hpText.text = hp.ToString();
     }
 
     private void HandleInput(){
@@ -47,22 +50,38 @@ public class TrackController : MonoBehaviour
     }
 
     private void HandleText(int userInputNumber){
-        guesses.text +=$"{userInputNumber}\n";
         userGuess.text = "";
         //Equal
         if (numToGuess == userInputNumber){
+            guesses.text +=$"<color=green>{userInputNumber}\n";
             guessResult.text = "<color=green>O";
+            DeactivateTrack();
         }
         //bigger
         else if(numToGuess>userInputNumber){
+            guesses.text +=$"<color=red>{userInputNumber}\n";
             guessResult.text = "<color=red>X ↑";
-
+            HandleHP();
         }
         //Smaller
         else{
+            guesses.text +=$"<color=red>{userInputNumber}\n";
             guessResult.text = "<color=red>X ↓";
-
+            HandleHP();
         }
+    }
+
+    private void HandleHP(){
+        hp--;
+        hpText.text = hp.ToString();
+        if (hp==0){
+            DeactivateTrack();
+        }
+    }
+
+    private void DeactivateTrack(){
+        submitButton.interactable = false;
+        userGuess.interactable = false;
     }
 
     
