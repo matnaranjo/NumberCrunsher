32d3d9bfa72e115e95175947cfa3882ef7c438c7
HSAM-H <151456585+HSAM-H@users.noreply.github.com>
2025-04-08 15:46:46 -0300

    Intro

--- Assets/Scripts/UI/UIIntro.cs
+++ Assets/Scripts/UI/UIIntro.cs
@@ -1,5 +1,6 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIIntro : MonoBehaviour
 {
@@ -11,7 +12,17 @@ public class UIIntro : MonoBehaviour
     TextMeshProUGUI nameDisplay;  // TextMeshProUGUI component to display the user's name
     [SerializeField]
     GameObject clickToContinue;  // GameObject for the "click to continue" button
+    [SerializeField]
+    GameObject HowTo;  // GameObject for the HowTo panel that displays the instructions
 
+private void Start()
+    {
+        // Ensure the HowTo panel is hidden at the start
+        if (HowTo != null)
+        {
+            HowTo.SetActive(false);
+        }
+    }
     // Called when the user has already entered a name
     public void UserHasName(){
         nameInput.SetActive(false);  // Hide the name input screen
@@ -28,5 +39,20 @@ public class UIIntro : MonoBehaviour
     // Updates the name display with the given name
     public void GetNameAndDisplay(string name){
         nameDisplay.text = nameDisplay.text + name;  // Display the user's name on the UI
+    }
+
+    // Called when the HowTo button is clicked
+    public void OnButtonHowToClick()
+    {
+        welcome.SetActive(false);  // Hide the welcome screen
+        HowTo.SetActive(true);  // Show the HowTo panel with instructions
+        clickToContinue.SetActive(false);  // Hide the "click to continue" button while in HowTo panel
+    }
+
+    // Called when the Back button on the HowTo panel is clicked
+    public void OnBackClick()
+    {
+        // Load the Intro scene
+        SceneManager.LoadScene("Intro");
     }
 }
